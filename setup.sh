deaac78b68c9e3bed9a8269b27d09d64ed0148f0
alej <nerdalej@gmail.com>
2021-09-09 17:10:41 -0400

    completed setup.sh, but missing dev tools and download of dotfiles

--- setup.sh
+++ setup.sh
@@ -8,7 +8,8 @@ set -e
 #   - dmenu
 
 FOREING_TOOL_REPO_DIR="$HOME/foreing-tool-repos/"
-PGK_DIR="$HOME/installation-packages/"
+PKG_DIR="$HOME/installation-packages/"
+INSTALL_SCRIPT_DIR="$HOME/installation-scripts/"
 DRY_RUN=${DRY_RUN:-}
 for i in "$@"; do
     case $i in
@@ -39,6 +40,10 @@ command_exists() {
     command -v "$@" > /dev/null 2>&1
 }
 
+package_installed() {
+    dpkg -s "$@" > /dev/null 2>&1
+}
+
 is_dry_run() {
     if [ -z "$DRY_RUN" ]; then
         return 1
@@ -48,12 +53,13 @@ is_dry_run() {
 }
 
 user=$(id -un 2>/dev/null || true)
-sh_c="sh -c"
+sush_c="sh -c" # super user shell command
+sh_c="sh -c"  # user shell command
 if [ "$user" != 'root' ]; then
     if command_exists sudo; then
-        shc_c='sudo sh -c'
+        sush_c='sudo sh -c'
     elif command_exists su; then
-        sh_c='su -c'
+        sush_c='su -c'
     else
         echo "Error: this installer needs the ability to run commands as root." >&2
         echo "Unable to find either "sudo" or "su" available to make this happen." >&2
@@ -66,74 +72,85 @@ if [ "$user" != 'root' ]; then
 fi
 
 if is_dry_run; then
+    sush_c="echo"
     sh_c="echo"
 fi
 
-$sh_c "apt install sudo bash git vim neovim curl xmonad xmobar"
-
-#### Fonts
-NERD_FONTS_DIR="${FOREING_TOOL_REPO_DIR}nerd-fonts"
-if [ ! -d $FOREING_TOOL_REPO_DIR ]; then
+if [ ! -d "$FOREING_TOOL_REPO_DIR" ]; then
     $sh_c "mkdir $FOREING_TOOL_REPO_DIR"
 fi
-if [ ! -d "$NERD_FONTS_DIR" ]; then
-    $sh_c "git clone https://github.com/ryanoasis/nerd-fonts.git $NERD_FONTS_DIR"
-else
-    $sh_c "git -C $NERD_FONTS_DIR pull"
+if [ ! -d "$PKG_DIR" ]; then
+    $sh_c "mkdir $PKG_DIR"
 fi
 
-$sh_c "${NERD_FONTS_DIR}install.sh Mononoki" 
+$sush_c "apt install sudo bash git vim neovim curl xmonad xmobar suckless-tools"
+
+#### Fonts
+NERD_FONTS_DIR="${FOREING_TOOL_REPO_DIR}nerd-fonts/"
+if [ ! -d "$NERD_FONTS_DIR" ]; then
+    $sush_c "git clone https://github.com/ryanoasis/nerd-fonts.git $NERD_FONTS_DIR"
+else
+    $sush_c "git -C $NERD_FONTS_DIR pull"
+fi
+
+$sush_c "${NERD_FONTS_DIR}install.sh Mononoki" 
 
 #### Alacritty
 # Install rust compiler
-if [ ! command -v restup &> /dev/null ]; then
-    $sh_c "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
+RUST_BIN_DIR="$HOME/.cargo/bin/"
+if ! command_exists restup; then # or rustup bin dir doesnt exists
+    $sh_c "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y --profile minimal"
 fi
-$sh_c "rustup override set stable"
-$sh_c "rustup update stable"
+#$sh_c ". $HOME/.cargo/env"
+$sh_c "${RUST_BIN_DIR}rustup override set stable"
+$sh_c "${RUST_BIN_DIR}rustup update stable"
+# TODO: add rustup to bashrc
 
 # install deps
-$sh_c "apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3"
+$sush_c "apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3"
 
-#ALACRITTY_DIR="$FOREING_TOOL_REPO_DIR/alacritty"
-#if [ ! -d "$ALACRITTY_DIR" ]; then
-#    git clone https://github.com/alacritty/alacritty.git $ALACRITTY_DIR
-#    cd $ALACRITTY_DIR  #todo: try building from outside the alacrity repo
-#    cargo build --release
-#
-#    sudo cp target/release/alacritty /usr/bin
-#    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-#    sudo desktop-file-install extra/linux/Alacritty.desktop
-#    sudo update-desktop-database
-#    # man page
-#    sudo mkdir -p /usr/local/share/man/man1
-#    gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
-#    cd ..
-#fi
-## todo: add to dotfiles repo
-## mkdir -p ~/.bash_completion
-## cp extra/completions/alacritty.bash ~/.bash_completion/alacritty
-## echo "source ~/.bash_completion/alacritty" >> ~/.bashrc
-#
+ALACRITTY_DIR="${FOREING_TOOL_REPO_DIR}alacritty/"
+if [ ! -d "$ALACRITTY_DIR" ]; then
+    $sh_c "git clone https://github.com/alacritty/alacritty.git $ALACRITTY_DIR"
+    #cd $ALACRITTY_DIR  #todo: try building from outside the alacrity repo
+    $sh_c "${RUST_BIN_DIR}cargo build --release --manifest-path=${ALACRITTY_DIR}Cargo.toml"
+
+    $sush_c "cp ${ALACRITTY_DIR}target/release/alacritty /usr/bin"
+    $sush_c "cp ${ALACRITTY_DIR}extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg"
+    $sush_c "desktop-file-install ${ALACRITTY_DIR}extra/linux/Alacritty.desktop"
+    $sush_c "update-desktop-database"
+    # man page
+    $sush_c "mkdir -p /usr/local/share/man/man1"
+    $sush_c "gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null"
+fi
+# todo: add to dotfiles repo
+# TODO: add autocomplete  https://github.com/alacritty/alacritty/blob/master/INSTALL.md#install-the-rust-compiler-with-rustup
+# mkdir -p ~/.bash_completion
+# cp extra/completions/alacritty.bash ~/.bash_completion/alacritty
+# echo "source ~/.bash_completion/alacritty" >> ~/.bashrc
+
 ##### LSD: download from release page
-#curl --proto '=https' --tlsv1.2 -sSf https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd_0.20.1_amd64.deb -o $PKG_DIR/lsd.deb
-#sudo apt install $PGK/lsd.deb
-#
-##### Starship
-## Fetch and install the latest version of starship, if starship is already
-## installed it will be updated to the latest version.
-#sh -c "$(curl -fsSL https://starship.rs/install.sh)"
-#
-#
-## how about updating apt repo list for non-free libs
-## it requires some coordination between foreing keys stored in /uset/share/keyrings/
-##    and the repositories in /etc/apt/sources.list.d/ and 
-###  todo: update terminal selection to pick a default when allacritty is not available
-#
-##################
-##### DEV      ###
-##################
-#
+# curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/Peltoche/lsd/releases/latest
+## in case you'd like to programatically download the latest version
+if package_isntalled lsd; then
+    $sh_c "wget https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd_0.20.1_amd64.deb -O ${PKG_DIR}lsd.deb"
+fi
+$sush_c "apt install ${PKG_DIR}lsd.deb"
+
+#### Starship
+# Fetch and install the latest version of starship, if starship is already
+# installed it will be updated to the latest version.
+$sush_c  "curl -fsSL https://starship.rs/install.sh | sudo sh -- -y"
+
+# how about updating apt repo list for non-free libs
+# it requires some coordination between foreing keys stored in /uset/share/keyrings/
+#    and the repositories in /etc/apt/sources.list.d/ and 
+##  todo: update terminal selection to pick a default when allacritty is not available
+
+#################
+#### DEV      ###
+#################
+
 #sudo apt install nodejs npm python3 pip3
 #
 ##### yarn
@@ -146,6 +163,7 @@ $sh_c "apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-x
 ## alias python3 and pip3 to python and pip
 #
 ##### Docker
+# INSTALL_SCRIPT_DIR="$HOME/installation-scripts/"
 ## if file not here
 #curl -fsSL https://get.docker.com -o get-docker.sh
 #sudo sh get-docker.sh
@@ -157,7 +175,7 @@ $sh_c "apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-x
 #
 ## jetbrains toolbox app
 #curl -sS 'https://download-cdn.jetbrains.com/toolbox/jetbrains-toolbox-1.21.9712.tar.gz' -o $PKG_DIR/jetbrains-toolbox.tar.gz
-#sudo tar -xzf $PGK_DIR/jetbrains-toolbox.tar.gz -C /opt
+#sudo tar -xzf $PKG_DIR/jetbrains-toolbox.tar.gz -C /opt
 #
 #
 #
