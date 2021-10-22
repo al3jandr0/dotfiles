c55474b28b2a98c1cbb0d4d26f8bc409f6311d46
alej <nerdalej@gmail.com>
2021-10-22 12:33:04 -0400

    Adds path.sh which adds local binaries to PATH

---
+++ .config/path.sh
@@ -0,0 +1,24 @@
+# set PATH so it icnludes Aws Amplify binaries if they exists
+if [ -d "$HOME/.aplify/bin" ] ; then
+    PATH="$HOME/.amplify/bin:$PATH"
+fi
+
+# set PATH so it includes cabal-built (haskell) binaries if it exists
+if [ -d "$HOME/.cabal/bin" ] ; then
+    PATH="$HOME/.cabal/bin:$PATH"
+fi
+
+# sets PATH so it includes cargo tools (rust) if they exist
+if [ -d "$HOME/.cargo/bin" ] ; then
+    PATH="$HOME/.cargo/bin:$PATH"
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
