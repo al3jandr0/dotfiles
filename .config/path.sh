a3bfe5c0d20e1e897f2055755e3552d6cd7291d9
alej <nerdalej@gmail.com>
2021-10-27 17:33:00 -0400

    Avoids adding repeated directorues to path
    I did this before and commited it, and I dont understand why the repo
    was not updated.

--- .config/path.sh
+++ .config/path.sh
@@ -1,24 +1,24 @@
 # set PATH so it icnludes Aws Amplify binaries if they exists
-if [ -d "$HOME/.aplify/bin" ] ; then
+if [ -d "$HOME/.aplify/bin" ] && [[ $PATH != *"$HOME.aplify/bin"* ]]; then
     PATH="$HOME/.amplify/bin:$PATH"
 fi
 
 # set PATH so it includes cabal-built (haskell) binaries if it exists
-if [ -d "$HOME/.cabal/bin" ] ; then
+if [ -d "$HOME/.cabal/bin" ] && [[ $PATH != *"$HOME/.cabal/bin"* ]]; then
     PATH="$HOME/.cabal/bin:$PATH"
 fi
 
 # sets PATH so it includes cargo tools (rust) if they exist
-if [ -d "$HOME/.cargo/bin" ] ; then
+if [ -d "$HOME/.cargo/bin" ] && [[ $PATH != *"$HOME/.cargo/bin"* ]]; then
     PATH="$HOME/.cargo/bin:$PATH"
 fi
 
 # set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/bin" ] ; then
+if [ -d "$HOME/bin" ] && [[ $PATH != *"$HOME/bin"* ]]; then
     PATH="$HOME/bin:$PATH"
 fi
 
 # set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/.local/bin" ] ; then
+if [ -d "$HOME/.local/bin" ] && [[ $PATH != *"$HOME/.local/bin"* ]]; then
     PATH="$HOME/.local/bin:$PATH"
 fi
