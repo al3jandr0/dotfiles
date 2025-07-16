7617b2ba3b5f809baf680011eadbcefd957d6542
alej <nerdalej@gmail.com>
2025-07-16 08:46:55 -0400

    Fixes XDG config home directory name

--- setupv3.sh
+++ setupv3.sh
@@ -7,7 +7,7 @@ set -x
 ##  Directory reference guide:                                                                   ##
 ##  https://wiki.archlinux.org/title/XDG_Base_Directory                                          ##
 ###################################################################################################
-XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/config}
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
 XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 XDG_STATE_HOME=${XDG_STATE_HOME:-$HOME/.local/state}
